0ebde8882a4974f7320ee1b732f33bdd8cca2fb5
Jonghyun <zxc159285@gmail.com>
2026-04-16 15:36:25 +0900

    0416postgres0

---
+++ src/main/java/com/karina/app/KosmoPostApplication.java
@@ -0,0 +1,13 @@
+package com.karina.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosmoPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosmoPostApplication.class, args);
+	}
+
+}
